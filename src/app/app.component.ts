a7c9ec361afde06de5cafd5b888c346057474234
raghavan <raghavan.as@hotmail.co.in>
2017-11-11 22:12:01 +0530

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-login></app-login>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'app';
+}
